eab2503492041a9ee8375369ac19209402cbd997
Thomas <tmcothran@gmail.com>
2025-04-13 12:24:32 -0400

    Implement Model Checker (#5)
    Problem
    ======
    Behavioral programming lends itself to model checking, without being translated into e.g., TLA+, as described in Harel, David, et al. "Model-checking behavioral programs." Proceedings of the ninth ACM international conference on Embedded software. 2011.
    
    Pavlov's model lends itself towards model checking to an even greater degree than BPJ, because:
    
    1. Bthreads are in priority order automatically, meaning that we don't need to build a graph to move through the state space of bthreads.
    2. Bthreads are serializable
    
    Solution
    ======
    Implement a `check` namespace, supporting:
    
    1. passing `events` without the need to construct bthreads
    2. pluggable strategy to support everything from permutations of events to random sampling
    3. automated deadlock checks
    
    Some things that are still TODO, checking if a bprogram state has already been checked, and clojurescript tests.

---
+++ src/tech/thomascothran/pavlov/check.clj
@@ -0,0 +1,84 @@
+(ns tech.thomascothran.pavlov.check
+  (:require [tech.thomascothran.pavlov.bprogram.ephemeral
+             :as bp]
+            [tech.thomascothran.pavlov.bthread :as b]))
+
+(defn- make-deadlock-bthread
+  []
+  {:request [{:type ::deadlock
+              :terminal true
+              :invariant-violated true}]})
+
+(defn straight-sequence
+  [events]
+  [events])
+
+(comment
+  (straight-sequence [1 2 3]))
+
+(defn run
+  "Check a bprogram to see if it violates safety or
+  liveness properties.
+
+  Parameters:
+  -----------
+  - `:safety-bthreads`: these are the bthreads that
+    fire when a safety propert
+  - `:bthreads`: these are the main bthreads that make
+    up the bprogram.
+  - `:events`: this is a collection of events. Depending
+    on the `:strategy`, all permutations may be tested,
+    random generators may be used, or the events may be
+    run in order.
+  - `check-deadlock`: if true, check for deadlocks
+  - `:subscribers`: any extra subscribers to add. Use these
+    for logging or stubbing. Map of name to function.
+  - `:strategy`: the a function applied to `:events`. It
+    must return a sequence of sequences of events.
+    For example, `(partial into [])` will check only
+    the sequence passed in `:events`. If no `:strategy` is given
+    this is the default behavior.
+    `clojure.math.combinatorics/permutations` will check all permutations.
+
+
+  Return
+  ------
+  return `nil` if no safety property is violated, otherwise
+  returns a map of:
+
+  - `:path`: the path of events that led to the violation
+  - `:event`: the type of the event identifing the violation"
+  [m]
+  (let [safety-bthreads (get m :safety-bthreads [])
+        bthreads (get m :bthreads [])
+        check-deadlock (get m :check-deadlock)
+        subscribers (get m :subscribers {})
+        strategy (get m :strategy straight-sequence)
+        events-combos (strategy (get m :events []))]
+    (loop [events-combos' events-combos]
+      (when-let [events (first events-combos')]
+        (let [input-threads (mapv (fn [event]
+                                    (b/bids [{:request [event]}]))
+                                  events)
+              all-bthreads
+              (->> (if check-deadlock
+                     [bthreads input-threads
+                      [(make-deadlock-bthread)]]
+                     [bthreads input-threads])
+                   (reduce into safety-bthreads))
+
+              !a (atom [])
+
+              state-tracker (fn [x _]
+                              (swap! !a conj x))
+
+              result
+              @(bp/execute! all-bthreads
+                            {:subscribers
+                             (assoc subscribers
+                                    :state-tracker
+                                    state-tracker)})]
+          (if (get result :invariant-violated)
+            {:path @!a
+             :event (last @!a)}
+            (recur (rest events-combos'))))))))
---
+++ test/tech/thomascothran/pavlov/check_test.clj
@@ -0,0 +1,161 @@
+(ns tech.thomascothran.pavlov.check-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [tech.thomascothran.pavlov.check :as check]
+            [tech.thomascothran.pavlov.bthread :as b]
+            [tech.thomascothran.pavlov.event :as event]
+            [tech.thomascothran.pavlov.bprogram.ephemeral-test.bthreads :as tb]))
+
+(defn all-moves
+  "request all combinations of x moves"
+  [players]
+  (->> (for [x-coord  [0 1 2]
+             y-coord  [0 1 2]
+             player   players]
+         [x-coord y-coord player])
+       (map #(hash-map :type %))))
+
+(defn x-moves-bthread
+  []
+  (b/reprise {:request (into [] (all-moves [:x]))}))
+
+(comment
+  (-> (b/bid (x-moves-bthread) nil)
+      (b/bid nil)
+      (b/bid nil)))
+
+(defn make-naive-strategy
+  []
+  (b/reprise {:request (into [] (all-moves [:o]))}))
+
+(defn initial-tic-tac-toe-bthreads
+  []
+  (reduce into []
+          [(mapv tb/make-winning-bthreads tb/winning-event-set)
+           [(tb/make-enforce-turn-bthreads)]
+           (tb/make-no-double-placement-bthreads)
+           [(make-naive-strategy)]]))
+
+(comment
+  (mapv b/name (initial-tic-tac-toe-bthreads)))
+
+(def x-win-paths
+  (into #{};; for each winning path set
+        (filter (comp #(= :x %) last first))
+        tb/winning-event-set))
+
+(defn make-losing-safety-bthread
+  "terminate the program as violating a safety property
+  if player wins"
+  [winning-path]
+  (b/step
+   [::we-lost-bthreads winning-path]
+   (fn [{:keys [remaining-events] :as acc} event]
+     (let [event-type (event/type event)
+           remaining-events' (disj remaining-events event-type)
+           events-to-watch
+           (into #{} (map (fn [event] {:type event})
+                          winning-path))
+           default-bid {:wait-on events-to-watch}]
+
+       (println "hit with " event)
+       (println "event-type" event-type)
+       (println "remaining-events" remaining-events')
+
+       (cond (nil? event) ;; event is nil on initialization
+             [{:remaining-events (set winning-path)} default-bid]
+
+             ;; Terminate - they won :-(
+             (= remaining-events #{event-type})
+             [{:remaining-events remaining-events'}
+              {:request #{{:type :x-won
+                           :invariant-violated true
+                           :terminal true}}}]
+
+             :else
+             [(update acc :remaining-events disj (event/type event))
+              default-bid])))))
+
+(defn losing-bthreads
+  []
+  (mapv make-losing-safety-bthread x-win-paths))
+
+(defn safety-bthreads
+  []
+  (losing-bthreads))
+
+(deftest test-tic-tac-toe-x-wins
+  (let [{:keys [event path]}
+        (check/run {:safety-bthreads (safety-bthreads)
+                    :check-deadlock true
+                    :bthreads (initial-tic-tac-toe-bthreads)
+                    :events (all-moves [:x])})
+
+        {:keys [invariant-violated] event-type :type} event]
+
+    (is invariant-violated)
+
+    (is (= :x-won event-type))
+
+    (is (= [{:type [0 0 :x]}
+            {:type [0 1 :o]}
+            {:type [0 2 :x]}
+            {:type [1 0 :o]}
+            {:type [1 1 :x]}
+            {:type [1 2 :o]}
+            {:type [2 0 :x]}
+            {:invariant-violated true, :terminal true, :type :x-won}]
+           path))))
+
+(deftest test-run-deadlock
+  (let [bthreads [{:wait-on #{:godot}}]
+        events [{:type [:beckett :writes]}]
+
+        result (check/run {:bthreads bthreads
+                           :events events
+                           :check-deadlock true})
+        event (get result :event)
+        path (get result :path)]
+
+    (is (= ::check/deadlock (get event :type)))
+    (is (= 2 (count path)))))
+
+(deftest test-safety-properties-are-checked
+  (let [bthreads [{:request #{:godot}}]
+
+        safety-bthreads
+        [(b/bids [{:wait-on #{:godot}}
+                  {:request [{:type :oh!
+                              :terminal true
+                              :invariant-violated true}]}])]
+
+        result (check/run {:bthreads bthreads
+                           :check-deadlock true
+                           :safety-bthreads safety-bthreads})
+        event (get result :event)
+        path (get result :path)]
+
+    (is (= :oh! (get event :type)))
+    (is (= true (get event :invariant-violated)))
+    (is (= 2 (count path)))))
+
+(deftest test-happy-path
+  (let [bthreads
+        [(b/bids [{:request #{:a}}
+                  {:request [:b]}])
+         (b/bids [{:wait-on #{:b}}
+                  {:request [{:type :c
+                              :terminal true}]}])]
+
+        safety-bthreads
+        [(b/bids [{:wait-on #{:z}}
+                  {:request [{:type :no!
+                              :terminal true
+                              :invariant-violated true}]}])]
+
+        events [{:type :a}]
+
+        result (check/run {:bthreads bthreads
+                           :events events
+                           :check-deadlock true
+                           :safety-bthreads safety-bthreads})]
+    (is (nil? result))))
