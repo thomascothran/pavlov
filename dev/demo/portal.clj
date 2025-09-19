40fb009492f059e34c86a4aca0d8cb517c873b05
Thomas Cothran <tmcothran@gmail.com>
2025-09-19 10:56:45 -0400

    portal

--- dev/demo/portal.clj
+++ dev/demo/portal.clj
@@ -1,5 +1,6 @@
 (ns demo.portal
   (:require [tech.thomascothran.pavlov.bthread :as b]
+            [tech.thomascothran.pavlov.event :as e]
             [tech.thomascothran.pavlov.nav :as nav]
             [portal.api :as p]))
 
@@ -15,6 +16,20 @@
                      :numbers (b/bids [{:request #{1 2}}
                                        {:request #{3}}])})
       (def root-node
-        (nav/root bthreads))
+        (nav/root bthreads
+                  (fn [{event :pavlov/event
+                        branches :pavlov/branches}]
+                    [^{:portal.viewer/default :portal.viewer/hiccup}
+                     [:div
+                      (if-not event
+                        [:h1 "Initialized"]
+                        [:h1 "Event: " (e/type event)])]
+                     ^{:portal.viewer/default :portal.viewer/hiccup}
+                     [:h3 "Branches"]
+                     branches
+                     ^{:portal.viewer/default :portal.viewer/hiccup}
+                     [:hr]])))
 
-      (tap> root-node)))
+      (tap> root-node)
+      (:wrapped root-node)
+      (get-in root-node [:pavlov/bthreads])))
