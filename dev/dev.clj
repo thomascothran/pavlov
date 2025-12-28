68cded2b6a15e6b7eef98ff8f97f137d8556e865
Thomas Cothran <tmcothran@gmail.com>
2025-12-28 15:51:35 -0500

    Change nrepl port

--- dev/dev.clj
+++ dev/dev.clj
@@ -16,7 +16,7 @@
   (atom nil))
 
 (defn start-nrepl!
-  ([] (start-nrepl! {:port 7888}))
+  ([] (start-nrepl! {:port 9898}))
   ([{:keys [port]}]
    (spit ".nrepl-port" port)
    (reset! !nrepl-server
